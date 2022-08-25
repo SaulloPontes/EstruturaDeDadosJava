c86df5c238a931cd7bff4e1418ba724c8e0b7b0f
Saulo araujo <102762350+SaulloPontes@users.noreply.github.com>
2022-08-25 15:59:09 -0300

    Update Lista.java

--- Lista.java
+++ Lista.java
@@ -1,41 +1,118 @@
-public class Main {
+public class Lista {
 
-    public static void main(String[] args) throws Exception {
+     int[] vetorElementos;
+     int numeroElementos;
+
+    public Lista(int capacidade){
+        this.numeroElementos = 0;
+        this.vetorElementos = new int[capacidade];
+    }
 
 
-        Lista t = new Lista(5);
-        System.out.println(t.listaCheia());
-        System.out.println(t.listaVazia());
-        t.adicionarInicio(1);
-        t.adicionarInicio(2);
-        t.adicionarInicio(3);
-        System.out.println(t.numeroElementos);
-        System.out.println(t.listaCheia());
-        System.out.println(t.listaVazia());
-        t.adicionarFinal(4);
-        System.out.println(t.numeroElementos);
-        System.out.println(t.listaCheia());
-        System.out.println(t.listaVazia());
-        t.removerFinal();
-        System.out.println(t.numeroElementos);
-        t.removerInicio();
-        System.out.println(t.numeroElementos);
-        t.adicionarPosicao(10,3);
-        System.out.println(t.numeroElementos);
-        t.removerPosicao(0);
-        System.out.println(t.numeroElementos);
-        t.adicionarInicio(0);
-        t.adicionarInicio(0);
-        t.adicionarInicio(0);
-        System.out.println(t.numeroElementos);
-        System.out.println(t.listaCheia());
-        t.removerInicio();
-        t.removerInicio();
-        t.removerInicio();
-        t.removerInicio();
-        t.removerInicio();
-        System.out.println(t.numeroElementos);
-        System.out.println(t.listaVazia());
+    public boolean listaVazia(){
+        if (numeroElementos==0){
+            return true;
+        }else{
+            return  false;
+        }
+    }
+
+    public boolean listaCheia(){
+        if (numeroElementos>=vetorElementos.length){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    public void adicionarFinal(int elem){
+            if(listaCheia()){
+                System.out.println("estrutura cheia");
+            }else{
+                int indice = numeroElementos;
+                vetorElementos[indice] =elem;
+                numeroElementos = numeroElementos + 1;
+            }
+    }
+
+
+
+    public  int  removerFinal() throws Exception {
+
+        if (listaVazia()) {
+            throw new Exception("estrutura vazia");
+        } else {
+            int indice = numeroElementos - 1;
+            int elementoRemovido = vetorElementos[indice];
+            numeroElementos =  indice;
+            return  elementoRemovido;
+        }
+
+    }
+
+    public void adicionarInicio(int elem){
+        if(listaCheia()){
+            System.out.println("estrutura cheia");
+        }else{
+            for(int indice = numeroElementos;indice>0;indice--){
+                vetorElementos[indice] = vetorElementos[indice-1];
+            }
+            vetorElementos[0] = elem;
+            numeroElementos = numeroElementos +1;
+        }
+    }
+
+    public  int removerInicio() throws Exception {
+        if(listaVazia()){
+            throw new Exception("estrutura vazia");
+        }else{
+            int elementoRemovido = vetorElementos[0];
+            for(int indice =1;indice<numeroElementos-1;indice++){
+                vetorElementos[indice-1] = vetorElementos[indice];
+            }
+            numeroElementos = numeroElementos - 1;
+            return  elementoRemovido;
+        }
+    }
+
+    public void  adicionarPosicao(int elem, int i)  {
+        if(listaCheia()){
+            System.out.println("estrutura cheia");
+        }else{
+            if(i<=0){
+                adicionarInicio(elem);
+            }else if(i>=numeroElementos){
+                adicionarFinal(elem);
+            }else{
+                for(int indice=numeroElementos;indice>i;indice--){
+                    vetorElementos[indice] = vetorElementos[indice-1];
+                }
+                vetorElementos[i] = elem;
+                numeroElementos = numeroElementos + 1;
+            }
+        }
+    }
+
+    public  int removerPosicao(int i) throws  Exception{
+        if(listaVazia()){
+            throw new Exception("estrutura vazia");
+        }else{
+            if(i<=0){
+                return removerInicio();
+            }else if(i>=numeroElementos){
+                return removerFinal();
+            }else{
+                int elementoRemovido = vetorElementos[i];
+                for(int indice =i;indice<numeroElementos-1;indice++){
+                    vetorElementos[indice] = vetorElementos[indice+1];
+                }
+                numeroElementos = numeroElementos-1;
+                return  elementoRemovido;
+
+            }
+        }
     }
 
 }
+
+

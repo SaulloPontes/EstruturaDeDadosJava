4d41c69ec701fd84b6da503605080ea9fbf7bdaa
Saulo araujo <102762350+SaulloPontes@users.noreply.github.com>
2022-08-25 15:58:44 -0300

    Update Main.java
    v2

--- Main.java
+++ Main.java
@@ -1,15 +1,41 @@
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
 
 
-        Lista t = new Lista(10);
-        t.adicionarInicio(10);
-        t.adicionarInicio(11);
+        Lista t = new Lista(5);
+        System.out.println(t.listaCheia());
+        System.out.println(t.listaVazia());
+        t.adicionarInicio(1);
+        t.adicionarInicio(2);
+        t.adicionarInicio(3);
         System.out.println(t.numeroElementos);
-
-
-
+        System.out.println(t.listaCheia());
+        System.out.println(t.listaVazia());
+        t.adicionarFinal(4);
+        System.out.println(t.numeroElementos);
+        System.out.println(t.listaCheia());
+        System.out.println(t.listaVazia());
+        t.removerFinal();
+        System.out.println(t.numeroElementos);
+        t.removerInicio();
+        System.out.println(t.numeroElementos);
+        t.adicionarPosicao(10,3);
+        System.out.println(t.numeroElementos);
+        t.removerPosicao(0);
+        System.out.println(t.numeroElementos);
+        t.adicionarInicio(0);
+        t.adicionarInicio(0);
+        t.adicionarInicio(0);
+        System.out.println(t.numeroElementos);
+        System.out.println(t.listaCheia());
+        t.removerInicio();
+        t.removerInicio();
+        t.removerInicio();
+        t.removerInicio();
+        t.removerInicio();
+        System.out.println(t.numeroElementos);
+        System.out.println(t.listaVazia());
     }
 
 }

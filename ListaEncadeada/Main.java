e75e70a23ba627223d6ad9572718c299bb553268
Saulo araujo <102762350+SaulloPontes@users.noreply.github.com>
2022-09-01 09:10:17 -0300

    Create Main.java

---
+++ ListaEncadeada/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+
+
+
+        ListaEncadeada l = new ListaEncadeada();
+        System.out.println(l.Lista_vazia());
+        l.Adicionar_inicio(10);
+        System.out.println(l.Lista_vazia());
+        l.Adicionar_final(100);
+    }
+
+}

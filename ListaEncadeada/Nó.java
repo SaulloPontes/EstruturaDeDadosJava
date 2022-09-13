7c20f9f2abd41bd1b5d451c8e0cde20e5bd2a2db
Saulo araujo <102762350+SaulloPontes@users.noreply.github.com>
2022-09-12 23:01:05 -0300

    Create Nó.java

---
+++ ListaEncadeada/Nó.java
@@ -0,0 +1,13 @@
+public class Nó {
+
+     int elemento_armazenado;
+     Nó proximo_no;
+
+        public  Nó(int e){
+
+             this.elemento_armazenado = e;
+             this.proximo_no = null;
+        }
+
+
+}

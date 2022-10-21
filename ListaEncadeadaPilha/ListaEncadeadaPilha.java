f3ce817508e5ed928064ee7bb20d47a18f7ddbde
Saulo araujo <102762350+SaulloPontes@users.noreply.github.com>
2022-10-21 09:41:10 -0300

    Update ListaEncadeadaPilha.java

--- ListaEncadeadaPilha/ListaEncadeadaPilha.java
+++ ListaEncadeadaPilha/ListaEncadeadaPilha.java
@@ -14,26 +14,36 @@ public class ListaEncadeadaPilha {
         }
     }
 
-    public  void  Adicionar_inicio(int e){
+     public  void  Adicionar_final(int e){
         Nó novo = new Nó(e);
-        if(!Lista_vazia()){
-            novo.proximo_no = inicio_lista;
+        if(Lista_vazia()){
+            inicio_lista = novo;
+        }else{
+            Nó auxiliar = inicio_lista;
+            while (auxiliar.proximo_no!=null){
+                auxiliar = auxiliar.proximo_no;
+            }
+            auxiliar.proximo_no = novo;
         }
-        inicio_lista = novo;
     }
 
 
-
-    public Nó Remover_Inicio(){
+    public  Nó Remover_final(){
         Nó removido = null;
+        Nó auxiliar;
         if(Lista_vazia()){
-            System.out.println("Estrutura vazia");
+            System.out.println("estrutura vazia");
         }else{
-            removido = inicio_lista;
-            inicio_lista = inicio_lista.proximo_no;
+            removido = auxiliar = inicio_lista;
+            while (removido.proximo_no!=null){
+                auxiliar = removido;
+                removido = removido.proximo_no;
+            }
+            auxiliar.proximo_no = null;
         }
         return removido;
     }
+
 
 
 }
